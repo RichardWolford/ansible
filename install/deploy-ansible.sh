71e409f37e93e1d7642fe83d91195c500a2a12a8
Richard Wolford (HPE) <richard.wolford@hpe.com>
2017-03-09 10:05:33 -0500

    Updated ansible configuration script

--- install/deploy-ansible.sh
+++ install/deploy-ansible.sh
@@ -3,11 +3,27 @@
 wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
 rpm -ivh epel-release-7-9.noarch.rpm
 
-yum update -y
+# delete the RPM
+rm epel-release-7-9.noarch.rpm -f
 
 yum install ansible -y
+yum install git -y
 
-ssh-keygen -t rsa -b 4096 -C "rwolford@ansible" -N "" -f "/root/.ssh/id_rsa"
+yum update -y
+
+if [! -f "/root/.ssh/id_rsa"]; then
+	ssh-keygen -t rsa -b 4096 -C "rwolford@ansible" -N "" -f "/root/.ssh/id_rsa"
+fi
+
+# clone ansible repository
+if [! -d "/root/.ssh/id_rsa"]; then
+	mkdir /etc/ansible/git
+	git clone https://github.com/RichardWolford/ansible.git /etc/ansible/git/
+	
+else
+	cd /etc/ansible/git
+	git pull origin master	
+fi
 
 # copy the key to the ansible hosts
 # ssh-copy-id rwolford@10.0.1.4
